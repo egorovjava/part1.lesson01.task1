e9d0e4311f2ab8b5a3edfc2fb776efc94310413c
egorov_av <egorov_av@mail.ru>
2019-10-30 12:57:27 +0300

    applying code conventions

--- src/main/java/part1/lesson01/App.java
+++ src/main/java/part1/lesson01/App.java
@@ -3,10 +3,8 @@ package part1.lesson01;
 /**
  * Homework part 1, lesson 1, task 1
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
